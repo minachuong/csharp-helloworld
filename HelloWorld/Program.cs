d22f945495a66c7d9056639197ba0d2fa322c4ca
Mina Chuong <mina.chuong@gmail.com>
2017-10-21 16:03:50 -0700

    arrays, strings, enums

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -14,7 +14,44 @@ namespace HelloWorld
 
 			Calculator calculator = new Calculator();
 			var result = calculator.Add(3, 4);
-			Console.WriteLine(result);
+			//Console.WriteLine(result);
+
+			//var numbers = new int[3];
+			//numbers[1] = 1;
+			//Console.WriteLine(numbers[0]);
+			//Console.WriteLine(numbers[1]);
+			//Console.WriteLine(numbers[2]);
+			//int[] numbers = new int[3] { 1, 2, 3 };
+
+			var strings = new string[3];
+			Console.WriteLine("1");
+			Console.WriteLine(strings[0]);
+			Console.WriteLine("");
+			Console.WriteLine("A");
+			Console.WriteLine(strings[1]);
+			Console.WriteLine(strings[2]);
+
+			//string name = string.Format("{0} {1}", firstName, lastName);
+			var numbers = new int[3] { 1, 2, 3 };
+			string list = string.Join(", ", numbers);
+			Console.WriteLine(list);
+			Console.WriteLine(list[0]);
+
+			var someAnimal = Animal.Cat;
+			Console.WriteLine(someAnimal);
+			Console.WriteLine((int)someAnimal);
+			var animalId = 1;
+			Console.WriteLine((Animal)animalId);
+			// converting string to enum
+			var longNeck = "Giraffe";
+			var tallAnimal = (Animal)Enum.Parse(typeof(Animal), longNeck);
 		}
+		public enum Animal : byte
+		{
+			Dog = 1,
+			Cat = 2,
+			Giraffe = 3,
+		}
+
 	}
 }
